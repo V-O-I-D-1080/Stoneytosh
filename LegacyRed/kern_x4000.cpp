b7e06c9fa4ed76847ecb24d45da1a9fda21a19c2
Zormeister <57213163+Zormeister@users.noreply.github.com>
2023-04-03 07:09:28 +1000

    I was lied to

--- LegacyRed/kern_x4000.cpp
+++ LegacyRed/kern_x4000.cpp
@@ -33,7 +33,6 @@ bool X4000::processKext(KernelPatcher &patcher, size_t index, mach_vm_address_t 
 			{"__ZN31AMDRadeonX4000_AMDVIUVDHWEngineC1Ev", this->orgGFX8UVDEngineConstructor},
 			{"__ZN30AMDRadeonX4000_AMDVISAMUEngineC1Ev", this->orgGFX8SAMUEngineConstructor},
 			{"__ZN31AMDRadeonX4000_AMDVIVCEHWEngineC1Ev", this->orgGFX8VCEEngineConstructor},
-			{"__ZN32AMDRadeonX4000_AMDUVD6v3HWEngineC1Ev", this->orgUVD6v3EngineConstructor},
             {"__ZZN37AMDRadeonX4000_AMDGraphicsAccelerator19createAccelChannelsEbE12channelTypes", orgChannelTypes},
             {"__ZN28AMDRadeonX4000_AMDCIHardware32setupAndInitializeHWCapabilitiesEv",
                 this->orgSetupAndInitializeHWCapabilities},
@@ -94,13 +93,13 @@ bool X4000::wrapAllocateHWEngines(void *that) {
 		DBGLOG("x4000", "Using GFX8 Constructors");
 		callback->orgGFX8PM4EngineConstructor(getMember<void *>(that, 0x3B0) = IOMallocZero(0x198));
 		callback->orgGFX8SDMAEngineConstructor(getMember<void *>(that, 0x3B8) = IOMallocZero(0x100));
-		/** According to Wikipedia, Stoney uses UVD 6.3, unconfirmed however, we also only have one SDMA channel on Stoney */
+		/** Only one SDMA channel is present on Stoney APUs  */
 		if (LRed::callback->chipType == ChipType::Stoney) {
-			callback->orgUVD6v3EngineConstructor(getMember<void *>(that, 0x3D8) = IOMallocZero(0x2F0));
+			DBGLOG("hwlibs", "Using only 1 SDMA Engine for Stoney");
 		} else {
 			callback->orgGFX8SDMAEngineConstructor(getMember<void *>(that, 0x3C0) = IOMallocZero(0x100));
-			callback->orgGFX8UVDEngineConstructor(getMember<void *>(that, 0x3D8) = IOMallocZero(0x2F0));
 		}
+		callback->orgGFX8UVDEngineConstructor(getMember<void *>(that, 0x3D8) = IOMallocZero(0x2F0));
 		callback->orgGFX8SAMUEngineConstructor(getMember<void *>(that, 0x400) = IOMallocZero(0x1D0));
 		callback->orgGFX8VCEEngineConstructor(getMember<void *>(that, 0x3E8) = IOMallocZero(0x258));
 	} else if (LRed::callback->gfxVer == GFXVersion::Unknown) {
