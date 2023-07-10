b0b649373101195f445d13a79261bfca9618f947
Zormeister <57213163+Zormeister@users.noreply.github.com>
2023-07-10 22:16:38 +1000

    Attempt to get the number of connectors from AtiObjectInfoTableInterface_V1

--- LegacyRed/kern_support.cpp
+++ LegacyRed/kern_support.cpp
@@ -90,7 +90,8 @@ bool Support::doNotTestVram([[maybe_unused]] IOService *ctrl, [[maybe_unused]] u
 }
 
 IOReturn Support::wrapGetAtomConnectorInfo(void *that, uint32_t connector, AtomConnectorInfo *coninfo) {
-    DBGLOG("support", "getAtomConnectorInfo: connector %x", connector);
+    auto connectorCount = getMember<uint32_t>(that, 0x10);
+    DBGLOG("support", "getAtomConnectorInfo: connector %x, connectorCount: %x", connector, connectorCount);
     auto ret = FunctionCast(wrapGetAtomConnectorInfo, callback->orgGetAtomConnectorInfo)(that, connector, coninfo);
     DBGLOG("support", "getAtomConnectorInfo: returned %x", ret);
     return ret;
